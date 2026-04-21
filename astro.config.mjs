0fbb91b1618c66df57f82223519e79ec89eeb246
Shotaro Kamijo <eng.telur4@gmail.com>
2026-04-21 14:40:12 +0900

    modify astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://eng-telur4.github.io',
-  base: 'eng-telur4.github.io',
   vite: {
     plugins: [tailwindcss()]
   }
